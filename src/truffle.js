89131df4e2e81a714f9ecdb2a239bed832ab63d8
Marko Marinovic <markoman.marko@gmail.com>
2018-09-19 21:06:50 +0200

    added ownable

--- src/truffle.js
+++ src/truffle.js
@@ -1,20 +1,9 @@
-const HDWalletProvider = require("truffle-hdwallet-provider");
-
 module.exports = {
   networks: {
     local: {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*'
-    },
-    rinkeby: {
-      provider: function() {
-        return new HDWalletProvider(
-          'attend twist rally finger fetch any cake kiss uncle monitor glimpse fruit',
-          'https://rinkeby.infura.io/ZKxA2TsccPJIoWO5h7EO'
-        )
-      },
-      network_id: '3',
     }
   }
 };
